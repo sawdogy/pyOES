7395e44e552377b95ee62f472c23f9fbcc8b6072
myhrekg <myhrekg@ornl.gov>
2019-03-09 22:20:39 -0500

    Update oes.py

--- oes.py
+++ oes.py
@@ -1,3 +1,57 @@
-import json #JavaScript Object Notation for easy reading and writing of files
-import os # operating system interfaces
+#Need to add modules to import
 
+import periodictable as pt #may not need this if everything if own database is given
+
+
+'''
+Functions
+'''
+
+
+
+def get_database_data(file_name=''):
+  
+  if not os.path.exists(file_name):
+     raise IOError("File {} does not exist!".format(file_name))
+  df = pd.read_csv(file_name, headers=1)
+  return df
+
+def get_lowerdegen(database_file_name=''):
+  """retrieve the degeneracy value for the lower energy level of a transition for specific species
+  """
+  
+  if file_extension != '.csv':
+    raise IOError("File type must be '.csv'")
+  else:
+    _df = get_database_data(file_name=database_file_name)
+    
+
+
+
+def get_upperdegen
+
+def get_lowerenergy
+
+def get_upperenergy
+
+def get_radiativelifetime
+
+def get_ionizationenergy
+
+def calculate_partitionfunc(species, temperature, elecdens,):
+  """ Calculates the partition function of element
+  
+  """
+  
+  partitionfunc = upperdegen * npexp(- upperenergy / temperature) #will need to make this a summming function
+  
+  return partitionfunc
+
+def calculate_lineintensity (species, temperature, elecdens,):
+  """ Calculates the intensity of an emission line
+  
+  """
+  
+  lineintensity = speciesconc * radiativelifetime * (upperdegen / partitionfunc) * np.exp(- upperenergy / temperature)
+  
+  return np.array(lineintensity)
